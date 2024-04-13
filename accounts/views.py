0d12f7911be428378d32382898c100b630b9d007
Norbert Brett <roco_norbi@yahoo.com>
2024-04-13 14:15:29 +0100

    latest updates

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from contacts.models import Contact
+from hosts.models import Host
 
 
 def register(request):
