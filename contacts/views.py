a72485b13b08a9bc176ca5f00df3525540f4db0d
Norbert Brett <roco_norbi@yahoo.com>
2024-04-14 13:13:38 +0100

    latest updates

--- contacts/views.py
+++ contacts/views.py
@@ -29,13 +29,13 @@ def contact(request):
         contact.save()
 
         # Send email
-        # send_mail(
-        #   'Property Listing Inquiry',
-        #   'There has been an inquiry for ' + listing + '. Sign in to the admin panel for more info',
-        #   'traversy.brad@gmail.com',
-        #   [host_email, 'techguyinfo@gmail.com'],
-        #   fail_silently=False
-        # )
+        send_mail(
+          'Property Listing Inquiry',
+          'There has been an inquiry for ' + listing + '. Sign in to the admin panel for more info',
+          'roco_norbi@yahoo.com',
+          [host_email],
+          fail_silently=False
+        )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/' + listing_id)
