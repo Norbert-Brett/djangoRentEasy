7659e2c0d25d5ac4e784b56829fc6e9856a34fd7
Norbert-Brett <roco_norbi@yahoo.com>
2026-02-02 19:08:57 +0100

    Fix: Move django_browser_reload URL to DEBUG-only block

--- djangorenteasy/urls.py
+++ djangorenteasy/urls.py
@@ -3,22 +3,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-admin_url = os.getenv('ADMIN_URL')
+
+admin_url = os.getenv('ADMIN_URL', 'admin/')
 
 urlpatterns = [
-                  path('', include('pages.urls')),
-                  path('listings/', include('listings.urls')),
-                  path('contacts/', include('contacts.urls')),
-                  path('accounts/', include('accounts.urls')),
-                  path('hosts/', include('hosts.urls')),
-                  path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
-                  path(admin_url, admin.site.urls),
-                  path("__reload__/", include("django_browser_reload.urls")),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('contacts/', include('contacts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('hosts/', include('hosts.urls')),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path(admin_url, admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-                      path("__debug__/", include(debug_toolbar.urls)),
-                  ] + urlpatterns
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ] + urlpatterns
